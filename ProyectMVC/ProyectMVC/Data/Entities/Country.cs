c1c8d5ab6af80f6b95f6c147afc816475c51db63
Luis David Lopera <luis.lopera28@gmail.com>
2022-03-14 14:43:39 -0500

    part 004 is complete

---
+++ ProyectMVC/ProyectMVC/Data/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectMVC.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="pais")]
+        [MaxLength(255, ErrorMessage = " ingresa los caracteres necesarios")]
+        [Required (ErrorMessage ="Ingrese el campo requerido")]
+        public int Name { get; set; }
+
+    }
+}
